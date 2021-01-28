1682b64a762445ff7fd7beff85652ac980fb9a25
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-28 17:56:07 +0000

    Adding delete records into controller

--- app.rb
+++ app.rb
@@ -17,5 +17,10 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  post '/bookmarks/delete' do
+    Bookmark.delete(params[:id])
+    redirect '/bookmarks'
+  end
+
   run! if app_file == $0
 end
